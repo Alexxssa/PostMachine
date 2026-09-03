1b8e1e2bd31d8af1a63cc9f7b6b13a0dae732438
Alex <alex@add.dp.ua>
2026-09-03 13:14:57 +0300

    Workflow

---
+++ PostMachine.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PostMachine.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("gypnor")]
+[assembly: AssemblyProduct("PostMachine.Tests")]
+[assembly: AssemblyCopyright("Copyright © gypnor 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d88afc9f-c978-4be8-843c-57bc5e6fbe37")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
