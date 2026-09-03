d550d2ee16f569e5a2809f10ebe907b65714f7bb
Alex <alex@add.dp.ua>
2026-09-03 11:52:56 +0300

    Додано нову фічу2: NewFeature

---
+++ PostMachine.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PostMachine.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("gypnor")]
+[assembly: AssemblyProduct("PostMachine.Tests")]
+[assembly: AssemblyCopyright("Copyright © gypnor 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2ee57beb-6c1f-4a89-b955-91a866f01f76")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PostMachine.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PostMachine.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            Assert.Equals(1,1);
+        }
+    }
+}
